f6dd4215e5f24138e83a37d7e42eb6f2899e3617
Iruoma Onyia <Iruomaonyia@outlook.com>
2022-04-03 04:15:02 +0100

    First Commit of Week 4 Assignments

---
+++ Methods/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Humanizer;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void GetQuadraticRoots()
+        {
+            Console.WriteLine("To get the roots of equations of type ax^2+bx+C");
+            Console.WriteLine();
+            Console.WriteLine("Input a value for a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input a value for b");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input a value for c");
+            double c = double.Parse(Console.ReadLine());
+            double x1;
+            double x2;
+            x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+            x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / 2 * a;
+            if ((Math.Pow(b, 2) - 4 * a * c) < 0)
+            {
+                x1 = double.NaN;
+                x2 = double.NaN;
+            }
+            Console.WriteLine($"The roots of the equation are {{{x1},{x2}}}");
+        }
+        static int GetFactorial(int number)
+        {
+            int factorial = 1;
+            while(number > 0)
+            {
+                factorial *= number;
+                number--;
+            }
+            return factorial;
+
+        }
+        static string GetWordNumber(int number)
+        {
+            string wordNumber;
+            if (number >= 1 && number <= 100)
+            {
+              wordNumber =  number.ToWords();
+                return wordNumber;
+            }
+            else
+            {
+                return "Please enter a number between 1 and 100";
+            }
+        }
+        static int TotalNumberOfChars(string stringValue)
+        {
+            return stringValue.Length;
+        }
+    }
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int length = GetStringLength("Iruoma");
+            //Console.WriteLine($"Length of the String is {length}"); 
+            //PrintPrimeNumbers(1,20);
+            //RandomNumbers(1, 20);
+            
+        }
+        static int GetStringLength(string inputString,int indexNumber=0)
+        {
+         
+            if (indexNumber == inputString.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                
+                int lengthOfString = 1 + GetStringLength(inputString, indexNumber+1);
+                Console.WriteLine($"Character {indexNumber+1} is {inputString[indexNumber]}");
+                return lengthOfString;
+            }
+            
+        }
+
+        static void PrintPrimeNumbers(int start, int end)
+        {
+            if (start > end)
+            {
+                return;
+            }
+            else
+            {
+                List<int> factors = new List<int>();
+                for (int i = 1; i < end+1; i++)
+                {
+                    if ((start % i) == 0)
+                    {
+                        factors.Add(i);
+                    }
+                    
+                }
+                if (factors.Count < 3)
+                {
+                    Console.WriteLine(start);
+                }
+                PrintPrimeNumbers(start + 1,end);
+            }
+        }      
+
+        static void RandomNumbers(int start,int stop)
+        {
+            Console.WriteLine($"Some random numbers from {start} to {stop} are;");
+            bool isNotRepeated = true;
+            List<int> AllRandomNumbers = new List<int>();
+            Random random = new Random();
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int randomNumber = random.Next(start, stop);
+            Console.WriteLine(randomNumber);
+            while (isNotRepeated)
+            {
+                AllRandomNumbers.Add(randomNumber);
+                randomNumber = random.Next(start, stop);
+                if (AllRandomNumbers.Contains(randomNumber))
+                {
+                    isNotRepeated = false;
+                }
+                
+                Console.WriteLine(randomNumber);
+            }
+            timer.Start();
+            Console.WriteLine();
+            Console.WriteLine($"{randomNumber} is repeated in {timer.ElapsedMilliseconds} milliseconds");
+        }
+    }
+}
